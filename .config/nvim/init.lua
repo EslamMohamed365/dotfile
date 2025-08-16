9468235d8d91f8328a38906d2e70373e1b51826a
Eslam Mohamed <am5484452@gmail.com>
2025-08-16 19:23:51 +0300

    refactor: clean up Neovim configuration and update plugin settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-local vim = vim
-local Plug = vim.fn["plug#"]
-
-vim.call("plug#begin")
-Plug("shaunsingh/nord.nvim")
-vim.call("plug#end")
---Lua:
-vim.cmd([[colorscheme nord]])
